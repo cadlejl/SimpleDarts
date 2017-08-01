9aa872517cc2208ac5199caea7be3ba7775b037a
Jaime Cadle <cadlejl@yahoo.com>
2017-08-01 15:24:41 -0500

    Added random and int properties to Darts

---
+++ Darts/Dart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darts
+{
+    public class Dart
+    {
+        public Random Random { get; set; }
+
+        private int randomInt;
+        public int RandomInt
+        {
+            get { return randomInt; }
+            set {
+                int RandomInt = Random.Next();
+
+                if (RandomInt <= 20 || RandomInt >= 0)
+                    randomInt = RandomInt;
+                else
+                    throw new Exception();
+            }
+        }
+    }
+}
