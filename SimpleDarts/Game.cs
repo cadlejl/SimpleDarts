8270a642ff6e3e2ea5c5b1d3d85c1e606f05168d
Jaime Cadle <cadlejl@yahoo.com>
2017-08-01 18:01:48 -0500

    Begin building Game class

--- SimpleDarts/Game.cs
+++ SimpleDarts/Game.cs
@@ -7,6 +7,22 @@ namespace SimpleDarts
 {
     public class Game
     {
+        public string Player1 { get; set; }
+        public string Player2 { get; set; }
+        public int Player1Score { get; set; }
+        public int Player2Score { get; set; }
 
+        public Game()
+        {
+            this.Player1 = "Player 1";
+            this.Player2 = "Player 2";
+            this.Player1Score = 0;
+            this.Player2Score = 0;
+        }
+
+        private void PlayOneGame()
+        {
+            Darts.Throw();
+        }
     }
 }
